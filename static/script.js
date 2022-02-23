400d31fd463ef662be691ba02ed80413e83fb536
Dale Harvey <daleharvey@daleharvey-u2q05p.home>
2022-02-23 21:14:41 +0000

    initial commit

---
+++ static/script.js
@@ -0,0 +1,32 @@
+let $ = document.querySelector.bind(document);
+
+let hashChanged = () => {
+  let src = document.location.hash.slice(1);
+  let dialog = $("#dialog");
+  if (src) {
+    $("#dialog img").src = src;
+    dialog.removeAttribute("hidden");
+    // Need to trigger a reflow to animate the
+    // dialog in.
+    dialog.offsetHeight;
+    $("#dialog").classList.remove("hidden");
+  } else if (!src && !dialog.hidden) {
+    dialog.addEventListener("transitionend", () => {
+      dialog.hidden = true;
+    }, { once: true });
+    dialog.classList.add("hidden");
+  }
+};
+
+$("#dialog").addEventListener("click", () => {
+  if (document.location.hash.length) {
+    // Doesn't trigger a scroll.
+    history.replaceState({}, "", "#");
+    hashChanged();
+  }
+}, false);
+
+
+window.addEventListener("hashchange", hashChanged, false);
+hashChanged();
+
